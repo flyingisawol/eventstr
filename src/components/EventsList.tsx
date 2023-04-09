7159ff569c1a31c6fa0c67d0b8ad23acf7dd79bc
andr3w <akeir@pm.me>
2023-04-10 07:59:31 +1000

    increase width of elements in EventCard and CreateEvent to 1/2

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -9,26 +9,24 @@ interface Props {
 
 const EventsList = ({ notes, metadata }: Props) => {
     return (
-        <>
-        <div className="flex flex-col gap-16 w-1/5">
+        <div className="flex flex-col gap-16 w-1/2">
             {notes.map((note) => (
-                <EventCard 
-                created_at={note.created_at}
-                user={{
-                    name: 
-                        metadata[note.pubkey]?.name ?? 
-                        `${nip19.npubEncode(note.pubkey).slice(0, 12)}...`,
-                    image: 
-                        metadata[note.pubkey]?.picture ?? 
-                        `https://api.dicebear.com/5.x/identicon/svg?seed=${note.pubkey}`,
-                    pubkey: note.pubkey,
-                }}
-                key={note.id} 
-                content={note.content} 
+                <EventCard
+                    created_at={note.created_at}
+                    user={{
+                        name:
+                            metadata[note.pubkey]?.name ??
+                            `${nip19.npubEncode(note.pubkey).slice(0, 12)}...`,
+                        image:
+                            metadata[note.pubkey]?.picture ??
+                            `https://api.dicebear.com/5.x/identicon/svg?seed=${note.pubkey}`,
+                        pubkey: note.pubkey,
+                    }}
+                    key={note.id}
+                    content={note.content}
                 />
             ))}
         </div>
-        </>
     );
 }
 
